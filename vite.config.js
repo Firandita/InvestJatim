37bb92c8e862c32909591c6595eafcf63f45122e
Firandita Yunia Rahmawati <firanditarahmawati@gmail.com>
2026-05-13 02:50:43 +0700

    fix: rebuild

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import fs from 'fs'
 
 export default defineConfig({
   plugins: [
     vue(),
+    {
+      name: 'generate-nojekyll',
+      closeBundle() {
+        fs.writeFileSync(resolve(__dirname, 'dist/.nojekyll'), '')
+      }
+    }
   ],
   base: '/InvestJatim/',
 })
